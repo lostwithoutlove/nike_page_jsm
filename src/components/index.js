bc2c571252b0f738a9a2a363453f0b63f5a2e62d
Abhilasha Purwar <abhilasha.purwar@gmail.com>
2024-02-11 10:52:43 +0530

    second commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Button from "./Button";
+import Nav from "./Nav";
+import PopularProductCard from "./PopularProductCard";
+import ShoeCard from "./ShoeCard";
+import ServiceCard from "./ServiceCard";
+
+export { Button, Nav, PopularProductCard, ShoeCard, ServiceCard };
